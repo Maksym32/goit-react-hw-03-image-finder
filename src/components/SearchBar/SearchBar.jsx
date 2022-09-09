2ea8520fb013910389bc07ce89a6a1bd2d8af3cf
Maksym32 <bednogo1@gmail.com>
2022-09-09 16:55:48 +0300

    hw3_image-finder

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,65 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+import { toast } from 'react-toastify';
+import { GoSearch } from "react-icons/go";
+import {
+  SearchHeader,
+  SearchForm,
+  SearchFormInput,
+  SearchButton,
+} from './SearchBar.styled';
+
+export class SearchBar extends Component {
+  static propTypes = {
+    onSubmit: PropTypes.func,
+  };
+
+  state = {
+    query: '',
+  };
+
+  handleChange = e => {
+    this.setState({ query: e.currentTarget.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    if (this.state.query.trim() === '') {
+      toast.warn('Please specify your query!');
+      return;
+    }
+    this.props.onSubmit(this.state.query);
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({ query: '' });
+  };
+
+  render() {
+    const { query } = this.state;
+
+    return (
+      <SearchHeader>
+        <SearchForm onSubmit={this.handleSubmit}>
+          <SearchButton type="submit">
+            <GoSearch style={{ width: 20, height: 20 }} />
+          </SearchButton>
+
+          <SearchFormInput
+            type="text"
+            name="query"
+            value={query}
+            onChange={this.handleChange}
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+          />
+        </SearchForm>
+      </SearchHeader>
+    );
+  }
+}
+
+
+
